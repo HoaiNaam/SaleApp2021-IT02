f156791de203b170c221cf1ecc50ef49717172c1
nampro10102002devil@gmail.com <nampro10102002devil@gmail.com>
2023-10-24 15:00:16 +0700

    cc

---
+++ saleappv1/dao.py
@@ -0,0 +1,40 @@
+def load_categories() :
+    return [{
+        'id' : 1,
+        'name' : "Mobile"
+    },{
+        'id': 2,
+        'name': "Tablet"
+    }]
+
+def load_product():
+    return [{
+        'id': 1,
+        'name': "Iphone 15",
+        'price': 3000000,
+        'image': 'https://cdn.viettelstore.vn/Images/Product/ProductImage/1349547788.jpeg'
+
+    },
+        {
+            'id': 1,
+            'name': "Iphone 15",
+            'price': 3000000,
+            'image': 'https://cdn.viettelstore.vn/Images/Product/ProductImage/1349547788.jpeg'
+        },
+        { 'id': 1,
+        'name': "Iphone 15",
+        'price': 3000000,
+        'image': 'https://cdn.viettelstore.vn/Images/Product/ProductImage/1349547788.jpeg'},
+
+        {
+        'id': 1,
+        'name': "Iphone 15",
+        'price': 3000000,
+        'image': 'https://cdn.viettelstore.vn/Images/Product/ProductImage/1349547788.jpeg'},
+
+        {'id': 1,
+        'name': "Iphone 15",
+        'price': 3000000,
+        'image': 'https://cdn.viettelstore.vn/Images/Product/ProductImage/1349547788.jpeg'}
+
+    ]
---
+++ saleappv1/index.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+import dao
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    cates = dao.load_categories()
+    product = dao.load_product()
+    return render_template("index.html", categories=cates, product=product)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
